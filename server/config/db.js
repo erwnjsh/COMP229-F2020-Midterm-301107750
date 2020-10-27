6d93b6d71d13d6a60d0a835bfae67f8026b64f2d
user <joshmanuel1996@gmail.com>
2020-10-27 11:42:58 -0400

    updated db connection string

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  //MongoAtlas Deployment ->
+  "URI": "mongodb+srv://erwin:5mrM9cINI8o2sJXe@f2020-comp229-midterm.kyutp.mongodb.net/books229?retryWrites=true&w=majority"
 };
