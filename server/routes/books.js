f7e240ca5b4d3173b7d0d8080b43d11082f2a825
user <joshmanuel1996@gmail.com>
2020-10-27 11:06:47 -0400

    completed delete route

--- server/routes/books.js
+++ server/routes/books.js
@@ -116,6 +116,19 @@ router.get('/delete/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    let id = req.params.id;
+    
+    book.remove({ _id: id }, (err) => {
+      if(err)
+      {
+        console.log(err);
+        res.end();
+      }
+      else
+      {
+        res.redirect('/books');
+      }
+    });
 });
 
 
